5f11aafa94620c12937de7602a6833cb8b964833
ytt8612 <ytt8612@gmail.com>
2015-01-21 11:11:34 +0800

    edit

--- ApiTest/test.php
+++
@@ -1,9 +0,0 @@
-<?php 
- //phpinfo();
- 
-set_time_limit(0);
-require ('resizeimage.php');
-$file =  new resizeimage("public/images/23_s.jpg", "200", "100", "0","public/images/1.jpg");
-var_dump($file);
-//$gmagick = new Gmagick_Handler();
-// $gmagick->make_thumb("public/images/23_s.jpg",200,300,true,80,"newimage/1.jpg");
